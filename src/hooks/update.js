d2abf91f5e9409faf150e348919bf75e1cd13ea4
shry <shry@users.noreply.github.com>
2020-04-25 13:14:37 -0300

    Added warning to update nofifier

--- src/hooks/update.js
+++ src/hooks/update.js
@@ -1,4 +1,5 @@
 const updateNotifier = require('update-notifier')
+const chalk = require('chalk')
 
 module.exports = async function (options) {
     const argv = options.argv || []
@@ -7,7 +8,22 @@ module.exports = async function (options) {
     const notifier = updateNotifier({pkg:pkg})
 
     if(argv.indexOf("--json") === -1){
-        //is-installed-globally could be used for npx installed packages
-        notifier.notify({isGlobal:true})
+
+        let message = 'Update available ' +
+			chalk.dim('{currentVersion}') +
+			chalk.reset(' → ') +
+			chalk.green('{latestVersion}') +
+			' \nRun ' + chalk.cyan('{updateCommand}') + ' to update';
+
+        if(notifier.update && notifier.update.type === 'major'){
+            message = message + 
+                    '\n\n CAUTION: This is a major version jump.' +
+                    '\n There are no guarantees about backwards compatibility.'
+        }
+
+        notifier.notify({
+            isGlobal:true,
+            message:message
+        })
     }
 }
